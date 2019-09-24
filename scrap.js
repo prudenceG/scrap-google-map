362d8f036268dd4c40e1189a56052530fd0f6daa
Sarawi Long <long.sarawi@gmail.com>
2019-09-24 12:52:55 +0200

    fetch name and address

--- scrap.js
+++ scrap.js
@@ -7,16 +7,23 @@ const place = args[1];
 
 getResultsFromGoogleMaps = (job, place) => {
   nightmare
+
   .goto('https://www.google.fr/maps')
   .type('#searchboxinput', `${job} ${place}`)
   .click('#searchbox-searchbutton')
   .wait('div.section-result')
-  .evaluate(() => document.querySelector('div.section-result h3.section-result-title span').innerHTML)
+  .evaluate(() => {
+    return Array.from(document
+      .querySelectorAll('h3.section-result-title, span.section-result-location'))
+      .map(element => element.innerText);
+  })
   .end()
   .then(console.log)
   .catch(error => {
     console.error('Search failed:', error)
   })
 }
+
+
 
 getResultsFromGoogleMaps(job, place);
